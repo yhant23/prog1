80ed1ebc187d909641d3d26bb210f9d9a8be9c40
yhant23 <32078917+yhant23@users.noreply.github.com>
2017-09-18 19:18:07 -0300

    Add files via upload

---
+++ Número Primo.c
@@ -0,0 +1,34 @@
+/****************************************************
+* Trab 2 : Numeros Primos
+* Nome e Matricula: Yhan Teixeira -> 0050015012
+* Professor: Alex Salgado
+*****************************************************/
+
+#include <stdio.h>
+
+int main ()
+{
+    int num;
+    int i;
+    int fat;
+    
+    do{
+       printf("\nEntre com um numero:");
+       
+       scanf("%d", &num);
+       
+}
+    while(num<=0);   
+    
+    // Calcular o fatorial;
+    fat = 1;
+    for(i=num; i>=1; i--)
+{
+      fat = fat * i;
+}
+      
+      printf("\nO fatorial = %d\n", fat);     
+      
+      system("pause");
+      return 0;
+}    
